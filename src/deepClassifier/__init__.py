3d497420270842bfe3a9f8b2a002e3230632a320
Samirsaiyed <samirsaiyed49@gmail.com>
2022-09-16 12:07:54 +0530

    updated

--- src/deepClassifier/__init__.py
+++ src/deepClassifier/__init__.py
@@ -1,4 +1,3 @@
-from asyncio.log import logger
 import os
 import sys
 import logging
@@ -9,11 +8,11 @@ log_filepath = os.path.join(log_dir, "running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.INFO, 
     format=logging_str,
     handlers=[
         logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout)
+        logging.StreamHandler(sys.stdout),
     ])
 
 logger = logging.getLogger("deepClassifierLogger")
